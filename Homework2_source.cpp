1d1210b483f2529e948595a3b48486e802011010
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-10-31 13:00:33 -0500

    Update Homework2_source.cpp

--- Homework2_source.cpp
+++ Homework2_source.cpp
@@ -1,47 +1,10 @@
-#include "DivSales.h"
-
-
-
-DivSales::DivSales()
+#include "Divsales.h"
+using namespace std;
+int main()
 {
-	for (int i = 0; i < 4; i++)
-	{
-		array[i] = 0;
-	}
-}
-
-void DivSales::quarterlySales(double first, double second, double third, double fourth)
-{
-	array[0] = first;
-	array[1] = second;
-	array[2] = third;
-	array[3] = fourth;
-	double sum = first + second + third + fourth;
-	totalsales += sum;
-
-}
-
-double * DivSales::quarterlySales()
-{
-
-	return array;
-}
-
-double DivSales::returnParticularsales(int i)
-{
-	double sales;
-	if (i >= 0 || i <= 3)
-	{
-		sales = array[i];
-	}
-	else
-	{
-		sales = -1;
-	}
-	return sales;
-}
-
-
-DivSales::~DivSales()
-{
-}
+	Divsales s;
+	s.quarterlySales(1, 2, 3, 4);
+	double * p= s.quarterlySales();
+	
+	
+	return 0;
