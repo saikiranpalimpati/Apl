8c1bf2472d35a88ebb899a539e31511245d4679f
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-10-31 13:14:12 -0500

    Update Homework2_source.cpp

--- Homework2_source.cpp
+++ Homework2_source.cpp
@@ -1,10 +1,16 @@
-#include "Divsales.h"
+#include "DivSales.h"
+#include<iostream>
 using namespace std;
 int main()
 {
-	Divsales s;
+	DivSales s;
 	s.quarterlySales(1, 2, 3, 4);
-	double * p= s.quarterlySales();
-	
-	
+	double * p=s.quarterlySales();
+	for (int i = 0; i < 4; i++)
+	{
+		cout << p[i] << endl;
+
+	}
+	system("pause");
 	return 0;
+}
