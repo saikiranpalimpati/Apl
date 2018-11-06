92b71aee22d64b4e959b6c71483015a47ca3bb20
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-11-06 13:06:21 -0600

    Update Homework2_Divsales.h

--- Homework2_Divsales.h
+++ Homework2_Divsales.h
@@ -1,15 +1,29 @@
 #pragma once
+
 class DivSales
-{
+{ 
+	//array to hold the sales of four quarters
 	double salesForDivision[4];
+	//variable to store the total of each division
+	double sum ;
+	//static variable to hold all the sales in each division
 	static double totalsales;
+
 public:
+	//default constructor
 	DivSales();
+	//method to initialise the values to array
 	void quarterlySales(double, double, double, double);
+	//method to return the array
 	double* quarterlySales();
-	//double returnParticularsales(int);
+	//method to return sales of particular qaurter
+	double returnParticularsales(int);
+	//method to return the total sales
+	double returnTotalsales();
 	~DivSales();
 };
+
+
 
 
 
